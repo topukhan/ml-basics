583ee7d353c8464038daec4e1b0190227c438628
topukhan <topukhan735@gmail.com>
2025-10-26 00:02:40 +0600

    added final step, gradient descent for model learning

--- main.py
+++ main.py
@@ -44,4 +44,45 @@ def compute_loss(y, Y_pred):
 
 #compute current loss
 loss = compute_loss(y, y_pred)
-print(f"Initial Loss: {loss:.4f}")
+# print(f"Initial Loss: {loss:.4f}")
+
+#step 7: compute gradients (derivatives)
+def compute_gradients(x, y, y_pred):
+    dw = (-2 / len(x)) * np.sum(x * (y - y_pred))
+    db = (-2 / len(x)) * np.sum(y - y_pred)
+    return dw, db
+
+#step 8: Train using gradient descent
+learning_rate = 0.001
+epochs = 10000
+
+for epoch in range(epochs):
+    #forward pass
+    y_pred = predict(x)
+
+    #compute loss
+    loss = compute_loss(y, y_pred)
+
+    #compute gradients
+    dw, db = compute_gradients(x, y, y_pred)
+
+    #update parameters
+    w -= learning_rate * dw
+    b -= learning_rate * db
+
+    # print progress occasionally
+    if epoch  % 100 == 0:
+        print(f"Epoch {epoch}: Loss={loss:.4f}, w={w[0]:.4f}, b={b[0]:.4f}")
+
+# step 9: final prediction after training
+y_final = predict(x)
+
+plt.scatter(x, y, color='blue', label="Actual Data")
+plt.plot(x, y_final, color='red', label="Learned line")
+plt.title("Model after training")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.legend()
+plt.show()
+
+print(f"\nFinal Parameters: w={w[0]:.4f}, b={b[0]:.4f}")
